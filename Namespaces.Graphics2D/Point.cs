63aa5c92fb89088871dc85b2f470cf7b482667cd
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-05-17 20:16:49 +0300

    Namespace aliases

---
+++ Namespaces.Graphics2D/Point.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Namespaces.Graphics2D
+{
+    public  class Point
+    {
+        public int X;
+        public int Y;
+    }
+}
---
+++ Namespaces.Main/Program.cs
@@ -0,0 +1,24 @@
+﻿using Point2D = Namespaces.Graphics2D.Point;
+using point3D= Namespaces.Graphics3D;
+using System;
+
+namespace Namespaces.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //aliasarea namespaceului
+
+            Point2D p2D = new Point2D();
+            p2D.X = 100;
+            p2D.Y = 200;
+
+            point3D::Point p3D = new point3D::Point();
+            p3D.X = 100;
+            p3D.Y = 200;
+            p3D.Z = 300;
+
+        }
+    }
+}
