ce34032407739d2f4e778af95cb185dc54b9db94
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-05-17 20:28:49 +0300

    Resolve a namespace and type conflict using global

---
+++ Namespaces.Main/ConsoleHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Namespaces.Main
+{
+    namespace System.Console
+    {
+        public class ConsoleHelper
+        {
+            public static void DoWork()
+            {
+                global::System.Console.WriteLine(".....");
+
+            }
+        }
+    }
+}
